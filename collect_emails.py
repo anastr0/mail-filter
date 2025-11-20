c85d07c5c7022492e43a7bea1f15aff4eadd0ef3
Anaswara T Rajan <anaswaratrajan@gmail.com>
2025-11-20 12:39:26 +0530

    collect emails from Gmail and store in Postgres

--- collect_emails.py
+++ collect_emails.py
@@ -1,12 +1,71 @@
+from datetime import datetime
+
 from google_auth_utils import get_gmail_api_service
 
 
-def collect_emails():
+def init_pg_conn():
+    """Initialize and return a PostgreSQL database connection."""
+    import psycopg2
+
+    conn = None
+    try:
+        conn = psycopg2.connect(
+            host="localhost", database="emaildb", user="atr", password="password"
+        )
+        print("Connected to PostgreSQL successfully!")
+    except psycopg2.Error as e:
+        print(f"Error connecting to PostgreSQL: {e}")
+    return conn
+
+
+def get_email_metadata(service, email_id):
+    """
+    Fetch email metadata from Gmail API for a given email ID.
+    Returns a tuple: (id, subject, from, to, date)
+    """
+    try:
+        metadata = (
+            service.users()
+            .messages()
+            .get(
+                userId="me",
+                id=email_id,
+                format="metadata",
+                metadataHeaders=["Subject", "From", "To", "Date"],
+            )
+            .execute()
+        )
+        header_map = {h["name"]: h["value"] for h in metadata["payload"]["headers"]}
+        received_date = header_map.get("Date", "")
+        if received_date:
+            received_date = datetime.strptime(
+                received_date[:31], "%a, %d %b %Y %H:%M:%S %z"
+            )
+
+        return (
+            metadata["id"],
+            header_map.get("Subject", ""),
+            header_map.get("From", ""),
+            header_map.get("To", ""),
+            received_date,
+        )
+    except Exception as e:
+        print(f"An error occurred while fetching email metadata: {e}")
+        return None
+
+
+def read_emails_from_gmail():
+    """
+    Fetch emails from Gmail using the Gmail API.
+    Returns email details a list of tuplesto be used in bulk insert query.
+    Each email detail tuple is : (id, subject, from, to, date)
+    """
     service = get_gmail_api_service()
     if not service:
         print("Failed to get Gmail API service.")
         return
 
+    emails = []
     try:
         # Call the Gmail API to fetch INBOX
         results = (
@@ -15,22 +74,43 @@ def collect_emails():
             .list(userId="me", labelIds=["INBOX"], maxResults=10)
             .execute()
         )
-        messages = results.get("messages", [])
+        emails = results.get("messages", [])
 
-        if not messages:
-            print("No messages found.")
+        if not emails:
+            print("No emails found.")
             return
-
-        print("Messages:", messages)
-        for message in messages:
-            msg = (
-                service.users().messages().get(userId="me", id=message["id"]).execute()
-            )
-            print(f"Message ID: {msg['id']}")
-
     except Exception as e:
-        print(f"An error occurred while fetching emails: {e}")
+        print(f"An error occurred while listing emails from Gmail API: {e}")
+        return
+
+    email_values = [get_email_metadata(service, email["id"]) for email in emails]
+    return email_values
+
+
+def store_emails_in_dbs():
+    """
+    Store emails fetched from Gmail into PostgreSQL database.
+    """
+    conn = init_pg_conn()
+    if not conn:
+        print("No database connection available.")
+        return
+
+    email_values = read_emails_from_gmail()
+    if not email_values:
+        print("No emails to store.")
+        return
+
+    insert_query = "INSERT INTO emails (id, subject_title, from_addr, to_addr, received_date) VALUES (%s, %s, %s, %s, %s);"
+    try:
+        with conn.cursor() as cursor:
+            cursor.executemany(insert_query, email_values)
+        conn.commit()
+    except Exception as e:
+        print(f"An error occurred while inserting emails into the database: {e}")
+    finally:
+        conn.close()
 
 
 if __name__ == "__main__":
-    collect_emails()
+    store_emails_in_dbs()
