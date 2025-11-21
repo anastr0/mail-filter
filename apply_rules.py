0c319c3056c012b24277860c77b1114e6c009cda
Anaswara T Rajan <anaswaratrajan@gmail.com>
2025-11-21 10:28:03 +0530

    read json ruleset and build filter query in SQL

--- apply_rules.py
+++ apply_rules.py
@@ -1,13 +1,21 @@
 from pprint import pprint
 
-from utils import init_pg_conn
+from utils import (
+    init_pg_conn,
+    RULES_VALIDATORS,
+    SQL_PREDICATES,
+    FIELD_ALIASES,
+    OPERATORS,
+    get_gmail_api_service,
+)
+
 
 def read_emails_from_db(conn):
-    cursor = conn.cursor()
-    cursor.execute(
-        "SELECT id, subject_title, from_addr, to_addr, received_date FROM emails"
-    )
-    rows = cursor.fetchall()
+    with conn.cursor() as cursor:
+        cursor.execute(
+            "SELECT id, subject_title, from_addr, to_addr, received_date FROM emails"
+        )
+        rows = cursor.fetchall()
     emails = [
         {
             "id": row[0],
@@ -20,6 +28,53 @@ def read_emails_from_db(conn):
     ]
     return emails
 
+
+def read_json_file():
+    import json
+
+    try:
+        with open("rules.json", "r") as file:
+            data = json.load(file)
+        print("JSON data loaded successfully:")
+        return data
+    except FileNotFoundError:
+        print("Error: 'data.json' not found. Please ensure the file exists.")
+    except json.JSONDecodeError:
+        print(
+            "Error: Could not decode JSON from the file. Check for valid JSON format."
+        )
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
+def build_condition(rule):
+    value = rule["value"]
+    field = rule["field"]
+    predicate = rule["predicate"]
+    condition = FIELD_ALIASES[field] + SQL_PREDICATES[predicate].format(value)
+    return condition
+
+
+def build_query(ruleset):
+    condition_list = [build_condition(rule) for rule in ruleset["rules"]]
+    op = OPERATORS[ruleset["overall_predicate"]]
+    condition = f" {op} ".join(condition_list)
+    query = f"SELECT id FROM emails WHERE ({condition})"
+    return query
+
+
+def read_rules():
+    data = read_json_file()
+
+    # TODO : validate rule set
+    # TODO : return all rulesets
+    return data["filters"][0]
+
+
+def apply_actions(actions):
+    pass
+
+
 def apply_rules():
     conn = init_pg_conn()
     emails = read_emails_from_db(conn)
@@ -27,6 +82,17 @@ def apply_rules():
     print("All stored emails")
     pprint(emails)
 
-if __name__=='__main__':
-    apply_rules()
+    ruleset = read_rules()
+    q = build_query(ruleset)
 
+    with conn.cursor() as cursor:
+        cursor.execute(q)
+        rows = cursor.fetchall()
+    conn.close()
+
+    # apply actions
+    apply_actions(ruleset["actions"])
+
+
+if __name__ == "__main__":
+    apply_rules()
